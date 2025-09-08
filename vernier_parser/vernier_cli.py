36094585048b6ee55e8d1954fc56593b0e286b5b
Rob Waters <rw796@cam.ac.uk>
2025-09-08 15:55:28 +0100

    Fixed a couple of issues with ruff

--- vernier_parser/vernier_cli.py
+++ vernier_parser/vernier_cli.py
@@ -3,8 +3,12 @@ A file for containing the vernier cli interface.
 """
 
 
-def main():
-    print('Need to make a cli interface')
+def main() -> None:
+    """
+    Vernier parser cli interface.
+    """
+    print("Need to make a cli interface")
+
 
 if __name__ == "__main__":
     main()
